90133e40b5f1a5847c8d66a894d14b688c2ed0bd
Yukihiro Fujita <buttix@gmail.com>
2012-04-15 19:47:39 +0900

    add ILogger

---
+++ mankitu/src/jp/tacores/mankitu/Logger.java
@@ -0,0 +1,30 @@
+package jp.tacores.mankitu;
+
+public class Logger implements ILogger {
+
+	public void d(String msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void e(String msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void i(String msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void v(String msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void w(String msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
