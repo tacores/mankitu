f87d0520f602b278f9e30a4a2e7faf955bfaa1a0
Yukihiro Fujita <buttix@gmail.com>
2012-04-15 20:08:37 +0900

    add IMessageBox

--- mankitu/src/jp/tacores/mankitu/Logger.java
+++ mankitu/src/jp/tacores/mankitu/Logger.java
@@ -1,6 +1,11 @@
 package jp.tacores.mankitu;
 
 public class Logger implements ILogger {
+	
+	public Logger(String tag) {
+		if(tag == null) throw new IllegalArgumentException("tag is null.");
+		this.tag = tag;
+	}
 
 	public void d(String msg) {
 		// TODO Auto-generated method stub
@@ -27,4 +32,5 @@ public class Logger implements ILogger {
 
 	}
 
+	private String tag;
 }
