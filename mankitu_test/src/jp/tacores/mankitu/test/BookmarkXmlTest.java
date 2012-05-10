2ab331a742d448b9ecfd7943df8c489499ebb16f
Yukihiro Fujita <buttix@gmail.com>
2012-05-10 22:49:05 +0900

    adding test methods.

--- mankitu_test/src/jp/tacores/mankitu/test/BookmarkXmlTest.java
+++ mankitu_test/src/jp/tacores/mankitu/test/BookmarkXmlTest.java
@@ -63,22 +63,79 @@ public class BookmarkXmlTest extends TestCase {
 		assertEquals("Progress list was not empty.", 0, sut.getProgressList().size());
 		assertEquals("Complete list was not empty.", 0, sut.getCompleteList().size());
 	}
-	public void testRetrieve_OneUnRead() {
-		BookmarkXml sut = new BookmarkXml(new XmlStreamStub(HEADER + UNREAD_BOOKMARK + FOOTER));
+	public void testRetrieve_UnRead_OneUnRead() {
+		String xml = HEADER + createUnReadBookmarkString() + FOOTER;
+		BookmarkXml sut = new BookmarkXml(new XmlStreamStub(xml));
 		
 		sut.retrieve(new NullContextContainer());
 		
 		assertEquals("UnRead list did't have one element.", 1, sut.getUnReadList().size());
 	}
+	public void testRetrieve_Waiting_OneProgress() {
+		String xml = HEADER + createWaitingBookmarkString() + FOOTER;
+		BookmarkXml sut = new BookmarkXml(new XmlStreamStub(xml));
+		
+		sut.retrieve(new NullContextContainer());
+		
+		assertEquals("Progress list did't have one element.", 1, sut.getProgressList().size());
+	}
+	public void testRetrieve_Reading_OneProgress() {
+		String xml = HEADER + createReadingBookmarkString() + FOOTER;
+		BookmarkXml sut = new BookmarkXml(new XmlStreamStub(xml));
+		
+		sut.retrieve(new NullContextContainer());
+		
+		assertEquals("Progress list did't have one element.", 1, sut.getProgressList().size());
+	}
+	public void testRetrieve_Complete_OneComplete() {
+		String xml = HEADER + createCompleteBookmarkString() + FOOTER;
+		BookmarkXml sut = new BookmarkXml(new XmlStreamStub(xml));
+		
+		sut.retrieve(new NullContextContainer());
+		
+		assertEquals("Complete list did't have one element.", 1, sut.getCompleteList().size());
+	}
+	public void testRetrieve_ContainAllStatus() {
+		String xml = HEADER +
+				createUnReadBookmarkString() +
+				createWaitingBookmarkString() +
+				createReadingBookmarkString() +
+				createCompleteBookmarkString() +
+				FOOTER;
+		BookmarkXml sut = new BookmarkXml(new XmlStreamStub(xml));
+		
+		sut.retrieve(new NullContextContainer());
+		
+		assertEquals("UnRead list was not 1.", 1, sut.getUnReadList().size());
+		assertEquals("Progress list was not 2.", 2, sut.getProgressList().size());
+		assertEquals("Complete list was not 1.", 1, sut.getCompleteList().size());
+	}
 	
 	private BookmarkXml createAnonBookmarkXml() {
 		return new BookmarkXml(new XmlStreamStub(ANON_XML));
 	}
+	private String createUnReadBookmarkString() {
+		return createAnonBookmarkElementWithoutEndTag() + "<status>ñ¢ì«</status>" + "</bookmark>";
+	}
+	private String createWaitingBookmarkString() {
+		return createAnonBookmarkElementWithoutEndTag() + "<status>êVäßë“Çø</status>" + "</bookmark>";
+	}
+	private String createReadingBookmarkString() {
+		return createAnonBookmarkElementWithoutEndTag() + "<status>ì«Ç›Ç©ÇØ</status>" + "</bookmark>";
+	}
+	private String createCompleteBookmarkString() {
+		return createAnonBookmarkElementWithoutEndTag() + "<status>äÆåã</status>" + "</bookmark>";
+	}
+	private String createAnonBookmarkElementWithoutEndTag() {
+		String str = "<bookmark>";
+		str += "<title>title</title>";
+		str += "<volume>4</volume><page>99</page>";
+		str += "<story>story</story><memo>memo</memo>";
+		str += "<date>20120509</date><uid>1234567890</uid>";
+		return str;
+	}
 	
 	final private String HEADER = "<bookmarks>";
 	final private String FOOTER = "</bookmarks>";
-	final private String ANON_XML = "";
-	final private String UNREAD_BOOKMARK = "<bookmark><title>title</title><status>ñ¢ì«</status>" +
-			"<volume>4</volume><page>99</page><story>story</story><memo>memo</memo>" +
-			"<date>20120509</date><uid>1234567890</uid></bookmark>";
+	final private String ANON_XML = "<bookmarks></bookmarks>";
 }
